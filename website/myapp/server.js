d482e0544ec92d0c649fd548e539c02c1e6b17cd
Charles Monaghan <charles@charlesmonaghan.com>
2021-04-05 00:41:47 +1200

    Added user Schema to allow mongoose database to interact

--- website/myapp/server.js
+++ website/myapp/server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const User = require('./model/user.js')
 const port = 3000
 
-mongoose.connect('mongodb://localhost:27017/login-app-db')
+mongoose.connect('mongodb://localhost:27017/login-app-db' {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}) 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
