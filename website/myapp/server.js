e4dacda7ec3fcdaaa6e7754271846f733b94739b
Charles Monaghan <charles@charlesmonaghan.com>
2021-04-05 00:12:54 +1200

    Installed body-parser and mongoose

--- website/myapp/server.js
+++ website/myapp/server.js
@@ -1,21 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const port = 3000
 
+mongoose.connect('mongodb://localhost:27017/login-app-db')
 app.use(express.static('public'))
+app.use(bodyParser.json())
+
+app.post('/api/register', async (req, res) => {
+  console.log(req.body)
+  res.json({ status: 'ok' })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-/*
-const express = require('express')
-const path = require('path')
-const port = 3000
-
-const app = express()
-app.use('/', express.static(path.join(__dirname, 'static')))
-
-app.listen(3000, () => {
-    console.log('Server up at port ' + port)
-})
-*/
