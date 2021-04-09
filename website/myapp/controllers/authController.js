ebde3bd216c11a4af468fd2f60324663951ee108
Charles Monaghan <charles@charlesmonaghan.com>
2021-04-09 16:46:06 +1200

    Converted project to EJS setup

---
+++ website/myapp/controllers/authController.js
@@ -0,0 +1,81 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code)
+    let errors = { email: '', password: '' }
+
+    // incorrect email
+    if (err.message === 'incorrect email') {
+      errors.email = 'that email is not registered'
+    }
+
+    // incorrect password
+    if (err.message === 'incorrect password') {
+      errors.password = 'that password is incorrect'
+    }
+
+    // duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered'
+        return errors;
+    }
+
+    // Validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message 
+        })
+    }
+
+    return errors
+}
+
+const maxAge = 3 * 24 * 60 * 60 // 3 days
+const createToken = (id) => {
+  return jwt.sign({ id }, 'nw8d395d243nj8h90!@#*&!)@(#*0wnp9m8edruq2o98i5', {
+    expiresIn: maxAge
+  }) // signs JWT
+}
+
+module.exports.signup_get = (req, res) => {
+  res.render('signup');
+}
+
+module.exports.login_get = (req, res) => {
+  res.render('login');
+}
+
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password })
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+    res.status(201).json({ user: user._id })
+  } catch (error) {
+    const errors = handleErrors(error)
+    res.status(400).json({ errors })
+  }
+}
+
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password)
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+    res.status(200).json({ user: user._id })
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors })
+  }
+}
+
+module.exports.logout_get = (req, res) => {
+  res.cookie('jwt', '', { maxAge: 1 }) // logs user out
+  res.redirect('/') // return to homepage
+}
--- website/myapp/server.js
+++ website/myapp/server.js
@@ -1,108 +1,28 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const User = require('./model/user.js')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
+const { requireAuth, checkUser } = require('./middleware/authMidderware')
 
-const JWT_SECRET = 'n980y2s!@#4980ndtq398as98aq3s@#$@4ua38p9uw'
+const app = express();
 
-mongoose.connect('mongodb://localhost:27017/login-app-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true // Removes deprecation warning
-}) 
+// middleware
 app.use(express.static('public'))
-app.use(bodyParser.json())
+app.use(express.json()); // Takes any json data, parses it into js
+app.use(cookieParser())
 
-app.post('/api/change-password', async (req,res) => {
-  const { token, newpassword: plainTextPassword } = req.body
+// view engine
+app.set('view engine', 'ejs')
 
-  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-    return res.json({ status: 'error', error: 'Invalid password' })
-  }
+// database connection
+const dbURI = 'mongodb://localhost:27017/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
+  console.log('Server open on localhost:3000')
 
-  if (plainTextPassword.length < 5) {
-    return res.json({ status: 'error', error: 'Password too small. Should be at least 6 characters.' })
-  }
-
-  try {
-    const user = jwt.verify(token, JWT_SECRET)
-    
-    const _id = user.id
-    
-    const password = await bcrypt.hash(plainTextPassword, 10)
-    
-    await User.updateOne(
-      { _id }, 
-      {
-      $set: { password }
-      }
-    )
-    res.json({ status: 'ok' })
-  } catch (error) {
-    console.log(error)
-    res.json({ status: 'error', error: ';))' })
-  }
-})
-
-app.post('/api/login', async (req, res) => {
-
-  const { username, password } = req.body
-  const user = await User.findOne({ username }).lean()
-
-  if (!user) {
-    return res.json({ status: 'error', error: 'Invalid, username/password'})
-  }
-
-  if (await bcrypt.compare(password, user.password)) {
-    // username, password combination is sucessfully
-
-    const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET)
-
-    return res.json({ status: 'ok', data: token })
-  }
-
-  res.json ({ status: 'error', error: 'Invalid, username/password' })
-})
-
-app.post('/api/register', async (req, res) => {
-  const { username, password: plainTextPassword } = req.body
-
-  if (!username || typeof username !== 'string') {
-    return res.json({ status: 'error', error: 'Invalid username' })
-  }
-
-  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-    return res.json({ status: 'error', error: 'Invalid password' })
-  }
-
-  if (plainTextPassword.length < 5) {
-    return res.json({ status: 'error', error: 'Password too small. Should be at least 6 characters.' })
-  }
-
-  const password = await bcrypt.hash(plainTextPassword, 10) // Hashes the password
-
-  try {
-    const response = await User.create({
-      username,
-      password
-    })
-    console.log('User created successfully: ', response)
-  } catch (error) {
-      if (error.code == 11000) {
-        // duplicate key
-        return res.json({ status: 'error', error: 'Username already in use' })
-      }
-      throw error
-  }
-    
-
-  res.json({ status: 'ok' })
-})
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+// routes
+app.get('*', checkUser)
+app.get('/', (req, res) => res.render('home'))
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'))
+app.use(authRoutes)
