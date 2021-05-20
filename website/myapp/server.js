d007ef01074463a5e1c693a23668893fa99d1f1b
RanulJaya <ranul.jaya@gmail.com>
2021-05-20 21:19:44 +1200

    Apply goes to the page of website from favourites and formatted the following files in prettier

--- website/myapp/server.js
+++ website/myapp/server.js
@@ -20,13 +20,13 @@ app.set('view engine', 'ejs')
 // database connection
 const dbURI = 'mongodb://localhost:27017/node-auth'
 mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err))
+    .connect(dbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
 console.log('Server open on localhost:3000')
 
 // routes
@@ -36,52 +36,52 @@ app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'))
 app.use(authRoutes)
 
 // job search funtionality
-const url = process.env.URI;
+const url = process.env.URI
 app.get('/data', function (req, res) {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err
-    let dbo = db.db('jobdb')
-    dbo
-      .collection('jobs')
-      .find({})
-      .toArray(function (err, result) {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err
-        res.json(result)
-        db.close()
-      })
-  })
-
-  // report/email funtionality
-  var email = 'jobspyreport@gmail.com';
-
-  var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: email,
-      pass: process.env.PASSWORD,
-    },
-  })
-
-  app.post('/report', (request, response) => {
-    const data = request.body.url
-    const userData = request.body.user
-    console.log(userData + ' ' + data + ' has been reported.')
-    var reportData = JSON.stringify(data)
-    const date = new Date();
-
-    var mailOptions = {
-      from: 'jobspyreport@gmail.com',
-      to: 'jobspyreport@gmail.com',
-      subject: 'Report',
-      text: userData + ' has reported ' + reportData + ' on ' + date + '.',
-    }
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log('Email sent: ' + info.response)
-      }
+        let dbo = db.db('jobdb')
+        dbo.collection('jobs')
+            .find({})
+            .toArray(function (err, result) {
+                if (err) throw err
+                res.json(result)
+                db.close()
+            })
     })
-  })
+
+    // report/email funtionality
+    var email = 'jobspyreport@gmail.com'
+
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: email,
+            pass: process.env.PASSWORD,
+        },
+    })
+
+    app.post('/report', (request, response) => {
+        const data = request.body.url
+        const userData = request.body.user
+        console.log(userData + ' ' + data + ' has been reported.')
+        var reportData = JSON.stringify(data)
+        const date = new Date()
+
+        var mailOptions = {
+            from: 'jobspyreport@gmail.com',
+            to: 'jobspyreport@gmail.com',
+            subject: 'Report',
+            text:
+                userData + ' has reported ' + reportData + ' on ' + date + '.',
+        }
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('Email sent: ' + info.response)
+            }
+        })
+    })
 })
